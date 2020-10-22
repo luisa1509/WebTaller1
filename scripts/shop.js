19639c96a5397cdc0ffb49ed203feba7f8cb6434
luisa1509 <luisa_15092000@hotmail.com>
2020-10-21 19:00:38 -0500

    html y js de las tienda creados

---
+++ scripts/shop.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    title: 'Celular Motorola Moto One Macro',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_671655-MCO43438392203_092020-V.webp',
+    price: 499900,
+  },
+  {
+    title: 'Celular Motorola One Action Color Blanco',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_760318-MCO42908273986_072020-V.webp',
+    price: 679900,
+  },
+  {
+    title: 'Celular Motorola Moto G8 Plus 64gb',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_846737-MCO43497294331_092020-V.webp',
+    price: 649900,
+  },
+  {
+    title: 'Motorola Moto G8 Power 4g',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_725226-MCO42143648422_062020-V.webp',
+    price: 699900,
+  },
+];
+
+const productsList = document.querySelector('.productslist');
