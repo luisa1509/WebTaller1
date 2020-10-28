9ce509e364ca6b369dedcb259153c86e2db1ceb0
luisa1509 <luisa_15092000@hotmail.com>
2020-10-28 00:00:37 -0500

    formulario de registro creado e inicializacion de firebase a parte

--- scripts/register.js
+++ scripts/register.js
@@ -1,10 +1,33 @@
+const db = firebase.firestore();
+const usersRef = db.collection('users');
+const register= document.querySelector('.register');
+
+register.addEventListener('.submit',function(event){
+    event.preventDefault();
+
+    const email = register.email.value;
+    const password = register.password.value;
 
 
-//registro de usuarios nuevos
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    //registro de usuarios nuevos
+firebase.auth().createUserWithEmailAndPassword(email, password)
+.then(function(credentials){
+
+ const uid = credentials.user.uid;
+ usersRef.doc(uid).set({
+    email:email,
+ }
+     
+ );
+ 
+
+})
+.catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
   });
   
+
+});
--- scripts/shop.js
+++ scripts/shop.js
@@ -1,15 +1,4 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyAUecKO45mFL5q7JNDkekeRa7YOFsZY5NM",
-  authDomain: "shop-wacom-js-project.firebaseapp.com",
-  databaseURL: "https://shop-wacom-js-project.firebaseio.com",
-  projectId: "shop-wacom-js-project",
-  storageBucket: "shop-wacom-js-project.appspot.com",
-  messagingSenderId: "891183012311",
-  appId: "1:891183012311:web:d58c75f0410104f239d392",
-  measurementId: "G-HBY8JKC8SS"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
 
 const db = firebase.firestore();
 const productsRef = db.collection('products');
