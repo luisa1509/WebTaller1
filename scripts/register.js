2e4a514bac2dd5e5c08e471e8fdc9e167fe23314
luisa1509 <luisa_15092000@hotmail.com>
2020-10-29 21:02:02 -0500

    arreglo al envio de usuarios y listas de ordenamientos pintadas

--- scripts/register.js
+++ scripts/register.js
@@ -1,37 +1,40 @@
 const db = firebase.firestore();
 const usersRef = db.collection('users');
-const register= document.querySelector('.register');
+const register = document.querySelector('.register');
 
-register.addEventListener('.submit',function(event){
-    event.preventDefault();
+register.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    const email = register.email.value;
-    const password = register.password.value;
+  const email = register.email.value;
+  const password = register.password.value;
 
 
-    //registro de usuarios nuevos
-firebase.auth().createUserWithEmailAndPassword(email, password)
-.then(function(credentials){
+  //registro de usuarios nuevos
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (credentials) {
 
-  console.log(credentials);
-  console.log(credentials.user.uid)
+      console.log(credentials);
+      console.log(credentials.user.uid)
 
- const uid = credentials.user.uid;
- usersRef.doc(uid).set({
-    email:email,
- })
- .then(function () {
-  window.location.href = 'shop.html';
-});
- 
+      const uid = credentials.user.uid;
+      usersRef.doc(uid).set({
+        email: email,
+      })
+        .then(function () {
+          window.location.href = 'shop.html';
+        });
 
-})
-.catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-  
+
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(error);
+
+      register.querySelector('.form__error').classList.remove('hidden');
+      // ...
+    });
+
 
 });
